64cd6832b08580e44c762870d419a8fd20135966
dnsdhrj <dnsdhrj123@gmail.com>
2018-09-11 20:55:08 +0900

    Refactor CLI with 'click' package

--- commands.py
+++ commands.py
@@ -1,34 +1,40 @@
-import sys
+import click
 
 from mail.blacklist import add_blacklist, remove_blacklist, show_blacklist
 from mail.feed import clear_feed
 
-argc = len(sys.argv)
-if argc < 2 or sys.argv[1] == '--help':
-    print('Commands list:')
-    print('  blackadd')
-    print('  blackdel')
-    print('  blacklist')
-    print('  clearemail')
-    exit()
+
+@click.group()
+def cli():
+    pass
 
 
-command = sys.argv[1]
-if command == 'blackadd':
-    if argc < 3:
-        print('Put arguments to add to email blacklist.')
-        exit()
-    for word in sys.argv[2:argc]:
+@cli.command(help='Add words to email blacklist.')
+@click.argument('words', required=True, nargs=-1)  # varargs
+def blackadd(words):
+    for word in words:
         add_blacklist(word)
         print('Added \'{}\' to the blacklist.'.format(word))
-elif command == 'blackdel':
-    if argc < 3:
-        print('Put arguments to delete in email blacklist.')
-        exit()
-    for word in sys.argv[2:argc]:
+
+
+@cli.command(help='Delete words in email blacklist.')
+@click.argument('words', required=True, nargs=-1)  # varargs
+def blackdel(words):
+    for word in words:
         remove_blacklist(word)
-        print('Deleted \'{}\' from the blacklist.'.format(word))
-elif command == 'blacklist':
+        print('Added \'{}\' to the blacklist.'.format(word))
+
+
+@cli.command(help='Show blacklisted words.')
+def blacklist():
     print(show_blacklist())
-elif command == 'clearemail':
+
+
+@cli.command(help='Clear all email notifications.')
+def clearemail():
     clear_feed()
+
+
+if __name__ == '__main__':
+    cli()
+
