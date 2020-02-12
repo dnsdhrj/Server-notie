f11b515d6d00e7cd140cfedf5647db8505cf219b
sonowz <dnsdhrj123@gmail.com>
2020-02-12 10:27:44 +0900

    Add one-liner scripts

--- default.nix
+++ default.nix
@@ -1,10 +1,33 @@
 with import <nixpkgs> {};
 
-python37Packages.buildPythonApplication {
-  pname = "notie";
-  version = "1.0";
-  src = lib.cleanSource ./.;
-  propagatedBuildInputs = callPackage ./requirements.nix {};
-  doCheck = false;
-}
+let
+  
+  # Actual Python package
+  notieBarebone = python37Packages.buildPythonApplication {
+    pname = "notie-barebone";
+    version = "1.0";
+    src = lib.cleanSource ./.;
+    propagatedBuildInputs = callPackage ./requirements.nix {};
+    doCheck = false;
+  };
 
+  # One-liners for convenience
+  blackaddScript = writeShellScriptBin "blackadd" "function _blackadd() { notie-command blackadd $1; }; _blackadd";
+  blackdelScript = writeShellScriptBin "blackdel" "function _blackdel() { notie-command blackdel $1; }; _blackdel";
+  blacklistScript = writeShellScriptBin "blacklist" "notie-command blacklist";
+  clearemailScript = writeShellScriptBin "clearemail" "notie-command clearemail";
+  mclearScript = writeShellScriptBin "mclear" "clearemail";
+
+in
+stdenv.mkDerivation {
+  name = "notie";
+  src = notieBarebone;
+  installPhase = ''
+    cp -r . $out
+    cp -r ${blackaddScript}/* $out
+    cp -r ${blackdelScript}/* $out
+    cp -r ${blacklistScript}/* $out
+    cp -r ${clearemailScript}/* $out
+    cp -r ${mclearScript}/* $out
+  '';
+}
